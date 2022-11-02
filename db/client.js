bcac7142cb9dd04e3810a8ee621bfceeb692498a
fitdizzle <markfitnessaz@gmail.com>
2022-11-02 14:39:33 -0700

    db env

--- db/client.js
+++ db/client.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017';
-const source = process.env.DATABASE;
+const source = process.env.DATABASE || 'attelier-product-db';
 
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
