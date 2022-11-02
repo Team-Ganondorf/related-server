3c534d2aa2579ba342272f506fe75a938da01903
fitdizzle <markfitnessaz@gmail.com>
2022-11-02 14:30:36 -0700

    testing mongo uri

--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const uri = process.env.MONGO_URI;
+const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017';
 const source = process.env.DATABASE;
 
 const client = new MongoClient(uri, { useUnifiedTopology: true });
