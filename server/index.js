6f7d7b527d918cc8c031763067076fd618e92a3c
fitdizzle <markfitnessaz@gmail.com>
2022-11-07 09:25:55 -0700

    adding loader key

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
   res.send('success');
 });
 
+app.get('/loaderio-eae136dd98e413d38d172053a20f0383.txt', (req, res) => {
+  res.send({ key: 'loaderio-eae136dd98e413d38d172053a20f0383' });
+});
+
 app.use(routes);
 
 const PORT = process.env.PORT | 8080;
