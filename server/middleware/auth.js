1e17919e152e1456d5daec8ef064a521a5a042c5
fitdizzle <markfitnessaz@gmail.com>
2022-11-07 10:26:12 -0700

    added error 401 to status

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,8 @@
 module.exports = async (req, res, next) => {
   const token = req.params.authorization;
   if (token !== process.env.SECRET_AUTH_KEY) {
-    res.end('Error not authorized');
+    res.status(401);
+    res.send('Error not authorized');
   } else {
     next();
   }
