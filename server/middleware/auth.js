b9a1471519966412fdd2a7cfb1624e435c292197
fitdizzle <markfitnessaz@gmail.com>
2022-11-07 10:29:58 -0700

    having trouble obtaining auth params. attempting req.query

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res, next) => {
-  const token = req.params.authorization;
+  const token = req.query.authorization;
   if (token !== process.env.SECRET_AUTH_KEY) {
     res.status(401);
     res.send('Error not authorized');
